a9fed67673751eb86d80802783fc4650bce15540
Norman Jeune III <normanjeuneiii@Normans-MacBook-Pro-2.local>
2021-08-19 22:50:00 -0400

    all tests passing except last 2

--- cypress/integration/form_test.js
+++ cypress/integration/form_test.js
@@ -6,9 +6,11 @@ describe('Quotes App', () => {
     //create helpers to collect dom elements
     const firstNameInput = () => cy.get(`input[name=first_name]`)
     const lastNameInput = () => cy.get(`input[name=last_name]`)
+    const foobarInput = () => cy.get (`input[name=foobar]`)
     const emailInput = () => cy.get(`input[name=email]`)
     const avatarInput = () => cy.get(`input[name=avatar]`)
     const termsOfServiceInput = () => cy.get(`input[name=termsOfService]`)
+    const submitBtn = () => cy.get(`button[id="submitBtn"]`)
 
     it('sanity check to make sure that tests work', () => {
         //keyword 'it' is a test
@@ -20,6 +22,15 @@ describe('Quotes App', () => {
         expect({}).not.to.equal({})// strict ===
         expect({}).to.eql({})  //not strict
     })
+
+    it('the proper elements are showing', () => {
+        firstNameInput().should('exist')
+        lastNameInput().should('exist')
+        foobarInput().should('not.exist')
+        emailInput().should('exist')
+        avatarInput().should('exist')
+        termsOfServiceInput().should('exist')
+      })
 
 
    describe(`Filling out the inputs and cancelling`, () => {
@@ -40,6 +51,30 @@ describe('Quotes App', () => {
           .should('have.value', '')
           .type('norm@norm.com')
           .should('have.value', 'norm@norm.com')
+      })
+   })
+
+   describe(`selecting the checkbox`, () => {
+      it(`can select the checkbox`, () => {
+          termsOfServiceInput()
+            cy.get('[type="checkbox"]').check()
+      })
+   })
+
+   describe(`user can submit form data`, () => {
+    it('the submit button enables when all inputs are filled out', () => {
+        firstNameInput().type('Norm')
+        lastNameInput().type('Jeune')
+        emailInput().type('norm@norm.com')
+        avatarInput()
+            cy.get('[type="file"]').attachFile({
+                fileContent: fileContent.toString(),
+                fileName: 'testPicture.png',
+                mimeType: 'image/png'
+            })
+        termsOfServiceInput()
+            cy.get('[type="checkbox"]').check()
+        submitBtn().should('not.be.disabled')
       })
    })
 })
